4aea240d156b3cda157f1ace31ea39b97fc1f375
prowler0 <kadirtuna1989@gmail.com>
2014-03-23 22:49:02 +0200

    eklendi

--- Emulator8086/src/steps/Stack.java
+++ Emulator8086/src/steps/Stack.java
@@ -6,6 +6,7 @@
 
 package steps;
 
+import emulator8086.Degisken;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,24 +15,24 @@ import java.util.List;
  * @author kadirtuna
  */
 public class Stack {
-    List<Integer>stackList = null;
+    List<StackElement>stackList = null;
     
     private static Stack stack = null;
     
     private Stack(){
-        stackList = new ArrayList<Integer>();
+        stackList = new ArrayList<StackElement>();
     }
     public static Stack getStack(){
         if(stack == null)
             stack = new Stack();
         return stack;
     }
-    public Integer pop(){
-        Integer result = stackList.get(stackList.size()-1);
+    public StackElement pop(){
+        StackElement result = stackList.get(stackList.size()-1);
         stackList.remove(stackList.size()-1);
         return result;
     }
-    public void push(Integer value){
+    public void push(StackElement value){
         stackList.add(value);
     }
     
---
+++ Emulator8086/src/steps/StackElement.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package steps;
+
+/**
+ *
+ * @author kadirtuna
+ */
+public class StackElement {
+    public int size;
+    public int value;
+
+    public StackElement(int size, int value) {
+        this.size = size;
+        this.value = value;
+    }
+}
