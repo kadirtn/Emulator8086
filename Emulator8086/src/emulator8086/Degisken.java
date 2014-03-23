6236200273ce401f0c562fadd94cf0e390a404ff
prowler0 <kadirtuna1989@gmail.com>
2014-03-23 18:06:55 +0200

    eklendi

--- Emulator8086/src/emulator8086/Degisken.java
+++ Emulator8086/src/emulator8086/Degisken.java
@@ -14,19 +14,26 @@ public class Degisken {
     public DegiskenTur tur;
     public String deger;
     public int value;//decimal değer
+    public int size;//byte büyüklüğü 1-2 byte
 
     public Degisken(int value) {
         tur = DegiskenTur.IMMEDIATE;
         this.value = value;
+        this.size = 0;
     }
 
     public Degisken(String reg) {
         tur = DegiskenTur.REGISTER;
         deger = reg;
+        if(reg.equals("AX") || reg.equals("BX") || reg.equals("CX") || reg.equals("DX"))
+            size = 2;
+        else
+            size = 1;
     }
-    public Degisken(String degiskenAdi, int index){
+    public Degisken(String degiskenAdi, int boyut, int index){
         tur = DegiskenTur.MEMORY;
         deger = degiskenAdi;
+        size = boyut;
         value = index;
     }
 
