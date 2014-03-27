84626587ae6e1c13cff2ceaa79154fb90c7a6d4d
prowler0 <kadirtuna1989@gmail.com>
2014-03-27 21:56:05 +0200

    uppercase düzeltildi

--- Emulator8086/src/emulator8086/EmulatorFrame.java
+++ Emulator8086/src/emulator8086/EmulatorFrame.java
@@ -46,8 +46,8 @@ public class EmulatorFrame extends javax.swing.JFrame {
         careTaker = new CareTaker();
 
         systemMemory = new MemoryView(1024);
-        functionMap = new HashMap<String, Integer>();
-        variableMap = new HashMap<String, Memory>();
+        functionMap = new HashMap<>();
+        variableMap = new HashMap<>();
         try{
         komutList = asmToLineList(listContent);
         } catch (Exception ex) {
@@ -513,19 +513,14 @@ public class EmulatorFrame extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EmulatorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EmulatorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EmulatorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(EmulatorFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new EmulatorFrame().setVisible(true);
             }
@@ -583,7 +578,7 @@ public class EmulatorFrame extends javax.swing.JFrame {
 
             st = new StringTokenizer(listContent[i], " ,");
 
-            List<String> tokens = new ArrayList<String>();
+            List<String> tokens = new ArrayList<>();
             while (st.hasMoreTokens()) {
                 tokens.add(st.nextToken());
             }
@@ -630,7 +625,7 @@ public class EmulatorFrame extends javax.swing.JFrame {
 
             st = new StringTokenizer(listContent[i], " ,");
 
-            List<String> tokens = new ArrayList<String>();
+            List<String> tokens = new ArrayList<>();
             while (st.hasMoreTokens()) {
                 tokens.add(st.nextToken());
             }
